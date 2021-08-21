6add1787cc956eab9103c94eb7b3df85b5e7f010
JulianP911 <j.padilla@uniandes.edu.co>
2021-08-21 18:20:12 -0500

    Final commit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const http = require('http');
 const urlProveedores = 'https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json';
 const urlClientes = 'https://gist.githubusercontent.com/josejbocanegra/986182ce2dd3e6246adcf960f9cda061/raw/f013c156f37c34117c0d4ba9779b15d427fb8dcd/clientes.json';
 
+// Desarrollo del ejercicio. Para esto se utilizo las librerias:
+// La libreria axios para obtener la informacion corresponidiente de los proveedores y clientes mediante promesas
+// La libreria fs para el manejo de archivos en este caso sobre el archivo index.html para poder anadir la informacion correspondiente a mostrar
+// La libreria http para la creacion del servido y configuracion del puerto 8081
 http.createServer((req, resp) => {
   if (req.url === "/api/proveedores") {
     axios.get(urlProveedores).then((res) => {
